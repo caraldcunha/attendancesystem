0b65163f09ad3e2b03a59804a6eeeac609ee0e40
caraldcunha <caraldcunha475@gmail.com>
2026-01-02 12:36:03 +0530

    Update database.py

--- backend/database.py
+++ backend/database.py
@@ -1,1 +1,19 @@
+import sqlite3
+
+def get_db():
+    return sqlite3.connect("data/students.db")
+
+def init_db():
+    conn = get_db()
+    c = conn.cursor()
+
+    c.execute("""
+    CREATE TABLE IF NOT EXISTS users (
+        email TEXT PRIMARY KEY,
+        password TEXT
+    )
+    """)
+
+    conn.commit()
+    conn.close()
 
